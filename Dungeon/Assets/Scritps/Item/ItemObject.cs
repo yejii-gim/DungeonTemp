2aca792a2959f617ab5173830c038ebc4db03aaf
yeji-gim <piggygim3@gmail.com>
2025-05-19 15:55:21 +0900

    feat: ItemInteraction 기능 추가
    ItemInteraction 기능 추가

---
+++ Dungeon/Assets/Scritps/Item/ItemObject.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IInteractable
+{
+    public string GetInteractPrompt();
+    public void OnInteract();
+}
+public class ItemObject : MonoBehaviour, IInteractable
+{
+    public ItemData ItemData;
+
+    public string GetInteractPrompt()
+    {
+        string str = $"{ItemData.displayName}\n{ItemData.description}";
+        return str;
+    }
+
+    public void OnInteract()
+    {
+        CharcterManager.Instance.player.itemData = ItemData;
+        CharcterManager.Instance.player.addItem?.Invoke();
+        Destroy(gameObject);
+    }
+}
---
+++ Dungeon/Assets/Scritps/ScritableObject/ItemData.cs
@@ -0,0 +1,41 @@
+using JetBrains.Annotations;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum ItemType
+{
+    Equipable,
+    Consumable,
+    Resource
+}
+public enum ConsumableType
+{
+    Health,
+    Hunger
+}
+[Serializable]
+public class ItemDataConsumable
+{
+    public ConsumableType type;
+    public float value;
+}
+
+[CreateAssetMenu(fileName = "Item", menuName = "NewItem")]
+public class ItemData : ScriptableObject
+{
+    [Header("Info")]
+    public string displayName;
+    public string description;
+    public ItemType type;
+    public Sprite Icon;
+    public GameObject dropPrefab;
+
+    [Header("Stacking")]
+    public bool canStack;
+    public int maxStackAmount;
+
+    [Header("ConumableType")]
+    public ItemDataConsumable[] consumables;
+}
